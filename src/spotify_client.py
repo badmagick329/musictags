7e6151363ceb470f84b076b98575df5c053a1203
badmagick <uzairfarooqi@gmail.com>
2023-11-24 19:05:39 +0000

    refactor

--- src/spotify_client.py
+++ src/spotify_client.py
@@ -25,29 +25,6 @@ class SpotifyClient:
         tracks = [Track(item) for item in results["tracks"]["items"]]
         return tracks
 
-    # Note: Spotipy search sucks
-    def __search_tracks(
-        self, artist: str, track: str, album: str = "", max: int = 15
-    ) -> list[Track]:
-        query_str = f"{track}"
-        if album:
-            query_str += f" album:{album}"
-        if artist:
-            query_str += f" artist:{artist}"
-        query = quote(query_str)
-        results = self.client.search(q=query, type="track", limit=max)
-        tracks = [Track(item) for item in results["tracks"]["items"]]
-        tracks = self.filter_tracks(tracks, artist)
-        prev_len = len(tracks)
-        while len(tracks) < max and results["tracks"].get("next", None):
-            results = self.client.next(results["tracks"])
-            tracks.extend([Track(item) for item in results["tracks"]["items"]])
-            tracks = self.filter_tracks(tracks, artist)
-            if len(tracks) == prev_len:
-                break
-            prev_len = len(tracks)
-        return tracks
-
     @staticmethod
     def __filter_tracks(tracks: list[Track], artist: str) -> list[Track]:
         return [
